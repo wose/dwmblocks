2f86f2e163116f6bb059fffc1ad3820f3f2be409
Sebastian Woetzel <wose@zuendmasse.de>
2020-12-04 03:52:26 +0100

    Adds some blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+#define WIDGET_PATH "~/.dwm/bin/"
+static const Block blocks[] = {
+    /*Icon*/ /*Command*/                                            /*Update Interval*/ /*Update Signal*/
+	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		            0},
+	{"", WIDGET_PATH "cpu",                                         5,                  0},
+	{"", WIDGET_PATH "ip",                                          60,                 0},
+	{"", WIDGET_PATH "weather",                                     300,                0},
+	{"", "date '+%a %d %b %Y %H:%M'",                               5,                  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
